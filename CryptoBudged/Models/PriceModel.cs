755627934f2eeb038209e4939d2b1a2325dc9331
Frederik Traber <frederik.traber@voicepoint.ch>
2017-12-26 15:04:08 +0100

    update todo; one file per class;

---
+++ CryptoBudged/Models/PriceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoBudged.Models
+{
+    public class PriceModel
+    {
+        public double BTC { get; set; }
+        public double USD { get; set; }
+        public double EUR { get; set; }
+        public double CHF { get; set; }
+        public double ETH { get; set; }
+    }
+}
--- CryptoBudged/Services/CryptoCurrencyService.cs
+++ CryptoBudged/Services/CryptoCurrencyService.cs
@@ -16,13 +16,4 @@ namespace CryptoBudged.Services
             }
         }
     }
-
-    public class PriceModel
-    {
-        public double BTC { get; set; }
-        public double USD { get; set; }
-        public double EUR { get; set; }
-        public double CHF { get; set; }
-        public double ETH { get; set; }
-    }
 }
